b392aa29deb310fd195cd3c177a27c698bc9656a
TzivyaAleph <rotlevy@g.jct.ac.il>
2021-10-07 12:36:24 +0300

    intial code+welcome functions

---
+++ Targil0/Program9945.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+     partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome9945();
+            welcome1550();
+            Console.ReadKey();
+        }
+
+        static partial void welcome1550();
+        private static void welcome9945()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", name);
+        }
+    }
+    
+}
