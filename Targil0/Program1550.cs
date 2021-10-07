aeda5d24ed7b0d1dfa5c1f466075e6f116d15112
yochevedsmile <yismailo@g.jct.ac.il>
2021-10-07 12:55:54 +0300

    more code+im here

---
+++ Targil0/Program1550.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void welcome1550()
+        {
+            Console.WriteLine("I'm also here!");
+
+        }
+    }
+}
