70eddf454b670b5b63ee0d84b9e6b36723f73df8
yochevedsmile <yismailo@g.jct.ac.il>
2021-10-17 14:07:03 +0300

    initial costumers

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -30,13 +30,22 @@ namespace DalObject
             init.stations[0] = new Station {lattitude=rnd.Next(0,1000),longitude= rnd.Next(0, 1000), stationName= "Ramot", chargeSlots= rnd.Next(0, 50) };
             init.stations[1] = new Station { lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000), stationName = "Har", chargeSlots = rnd.Next(0, 50) };
            
-            init.drones[0] = new Drone { ID = rnd.Next(0, 1000), model = "xPro", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery=rnd.Next(0,100) };
-            init.drones[1] = new Drone { ID = rnd.Next(0, 1000), model = "xMax", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery = rnd.Next(0, 100) };
-            init.drones[2] = new Drone { ID = rnd.Next(0, 1000), model = "xPlus", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery = rnd.Next(0, 100) };
-            init.drones[3] = new Drone { ID = rnd.Next(0, 1000), model = "zeo", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery = rnd.Next(0, 100) };
-            init.drones[4] = new Drone { ID = rnd.Next(0, 1000), model = "xox", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery = rnd.Next(0, 100) };
+            init.drones[0] = new Drone { ID = 11, model = "xPro", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery=rnd.Next(0,100) };
+            init.drones[1] = new Drone { ID = 22, model = "xMax", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery = rnd.Next(0, 100) };
+            init.drones[2] = new Drone { ID = 33, model = "xPlus", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery = rnd.Next(0, 100) };
+            init.drones[3] = new Drone { ID = 44, model = "zeo", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery = rnd.Next(0, 100) };
+            init.drones[4] = new Drone { ID = 55, model = "xox", maxWeight = (WeightCategories)rnd.Next(0, 2), status = (DroneStatuses)rnd.Next(0, 2), battery = rnd.Next(0, 100) };
 
-            
+            init.customers[0] = new Customer { ID = 111, name = "Avi", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
+            init.customers[1] = new Customer { ID = 222, name = "Bennie", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
+            init.customers[2] = new Customer { ID = 333, name = "Sahar", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
+            init.customers[3] = new Customer { ID = 444, name = "David", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
+            init.customers[4] = new Customer { ID = 555, name = "Guy", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
+            init.customers[5] = new Customer { ID = 666, name = "Justin", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
+            init.customers[6] = new Customer { ID = 777, name = "Bob", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
+            init.customers[7] = new Customer { ID = 888, name = "Alice", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
+            init.customers[8] = new Customer { ID = 999, name = "Noa", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
+            init.customers[9] = new Customer { ID = 1111, name = "Sara", phoneNumber = rnd.Next(0, 10000000), lattitude = rnd.Next(0, 1000), longitude = rnd.Next(0, 1000) };
 
         }
     }
